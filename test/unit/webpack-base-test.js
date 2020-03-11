4ed8b8f85c371273f2a0a3072c18bc8bb93d2680
sunyuhang404 <sunyuhang404@gmail.com>
2020-03-11 22:40:17 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/sunyuhang/Documents/WorkSpace-H5/webpack/test-01/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/sunyuhang/Documents/WorkSpace-H5/webpack/test-01/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('webpack-ci/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('webpack-ci/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
